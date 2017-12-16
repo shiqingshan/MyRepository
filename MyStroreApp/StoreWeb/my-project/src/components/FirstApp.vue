1e48be01061a2d4fe749de57039efce6dce2ba22
Hezw <1147863825@qq.com>
2017-12-16 21:37:24 +0800

    web部分

---
+++ MyStroreApp/StoreWeb/my-project/src/components/FirstApp.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+    import App from "../App";
+
+    export default {
+      components: {App},
+      name: "first-app"
+    }
+</script>
+
+<style scoped>
+
+</style>
