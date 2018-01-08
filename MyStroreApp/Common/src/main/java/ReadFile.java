b62ecc3d8467cf1e71ed0d98a33627d1bad85f57
Hezw <1147863825@qq.com>
2018-01-08 22:04:10 +0800

    二次提交

--- MyStroreApp/Common/src/main/java/ReadFile.java
+++ MyStroreApp/Common/src/main/java/ReadFile.java
@@ -18,21 +18,7 @@ public class ReadFile {
             while((line =br.readLine()) !=null)
 
             {
-                String[] str = line.split("\\|");
-                ssss = str[0].toString();
-                if(row > 1000){
-                    List list1=new ArrayList();
-                    if(row%1000==0){
-                        list1.add(ssss);
-                        ctl.save(list1);
-                        list1.clear();
-                    }else{
-                        list1.add(ssss);
-                    }
-                }else{
-                    list.add(ssss);
-                }
-                row++;
+
             }
            // ctl.save(list);
         } catch (FileNotFoundException e) {
